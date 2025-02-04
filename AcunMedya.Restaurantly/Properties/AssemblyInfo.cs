8ac516d8b010ae5dbf16047739da2d8e8771af19
Fatma Nur <135032308+fatmanurakbas@users.noreply.github.com>
2025-02-04 22:08:16 +0300

    Proje dosyası ekle.

---
+++ AcunMedya.Restaurantly/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir derleme hakkındaki genel Bilgiler aşağıdakiler üzerinden denetlenir
+// denetlenir. Bir derlemeyle ilişkilendirilmiş bilgileri değiştirmek için bu
+// öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("AcunMedya.Restaurantly")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AcunMedya.Restaurantly")]
+[assembly: AssemblyCopyright("Telif hakkı ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özelliğinin false olarak ayarlanması bu derlemedeki türleri COM bileşenlerine
+//COM görünmez kılar.  Bu derlemedeki bir türe şu kaynaktan erişmeniz gerekiyorsa:
+// COM, bu tür üzerinde ComVisible özniteliğini true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'a açılmışsa aşağıdaki GUID typelib'in ID'si içindir
+[assembly: Guid("a3f2d12d-9a63-4e68-91ef-6f877c6eb84a")]
+
+// Bir derlemenin sürüm bilgisi aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      Alt Sürüm
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya Gözden Geçirme ve Yapı Numaralarını varsayılan değerlerine döndürebilirsiniz
+// gösterildiği gibi '*' ile varsayılan değer atayabilirsiniz:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
